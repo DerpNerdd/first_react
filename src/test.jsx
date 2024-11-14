e674a0e17f98566b96d419bda4fea4157086bba7
Alan Sanchez <sanchez1.alan1@gmail.com>
2024-11-14 15:56:24 -0700

    React Website

---
+++ src/test.jsx
@@ -0,0 +1,23 @@
+import { MdLightbulb } from "react-icons/md";
+import { IoLogoDiscord } from "react-icons/io5";
+import Question from "./Question";
+
+function Test(){
+    const sport = "Baseball"
+    let src = "https://pbs.twimg.com/profile_images/1610231810201636870/Nj3OUXrQ_400x400.jpg"
+    let w = "250", h = "250", alt = "Oneshot Nico"
+    let info = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae nam totam, nesciunt fugit corrupti illo rem possimus voluptate reiciendis architecto."
+    return(
+        <>
+        <h1>Alan Sanchez</h1>
+        <p>Hello, my name is Alan Sanchez :3</p>
+        <h3>Boy do I love {sport} </h3>
+        <img src={src} width={w} height={h} alt= {alt} />
+        <button><MdLightbulb  size="250px" style= {{backgroundColor: "purple", color:"yellow"}} /></button>
+        <h1><IoLogoDiscord /></h1>
+        <Question title="Bojack Horseman" info={info} />
+        </>
+    )
+}
+
+export default Test;
